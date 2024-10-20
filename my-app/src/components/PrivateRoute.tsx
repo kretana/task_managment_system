0a17edf8062190a033e133bd4d783e8286e3cac7
Kretana <kretana.reveli@sisal.al>
2024-10-20 20:29:36 +0200

    token

--- my-app/src/components/PrivateRoute.tsx
+++ my-app/src/components/PrivateRoute.tsx
@@ -3,7 +3,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 
 const PrivateRoute: React.FC = () => {
-    // const { isAuthenticated } = useSelector((state: RootState | any) => state.auth);
     const token = localStorage.getItem('authToken')
     return token ? <Outlet /> : <Navigate to="/" replace />;
 };
--- my-app/src/screens/AuthForm.tsx
+++ my-app/src/screens/AuthForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import {useDispatch, useSelector} from "react-redux";
 import { login } from "../redux/slices/auth/authThunk";
@@ -14,6 +14,11 @@ export const AuthForm: React.FC = () => {
     const dispatch = useDispatch<AppDispatch>();
 
     const { loading, error } = useSelector((state: RootState | any) => state.auth);
+    const token = localStorage.getItem('authToken')
+
+    useEffect(() => {
+        if(token) navigate('/dashboard')
+    }, [token])
 
     const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -25,54 +30,56 @@ export const AuthForm: React.FC = () => {
         }
     };
 
-    return (
-        <div className="flex min-h-screen items-center justify-center bg-gradient-to-br px-4">
-            <div className="p-8 max-w-md w-full bg-white rounded-lg shadow-2xl">
-                <h2 className="text-3xl font-bold  mb-8 text-center">
-                    Log In
-                </h2>
-                <form onSubmit={handleLogin} className="space-y-8">
-                    <div>
-                        <Input
-                            label="Username"
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            placeholder="Enter your username"
-                            className="mt-4"
-                            labelClassName="text-black"
-                            inputClassName="mt-1 block w-full px-4 py-3 rounded-lg shadow-sm focus:outline-none transition duration-300 ease-in-out"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <Input
-                            label="Password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Enter your password"
-                            className="mt-4"
-                            labelClassName="text-black"
-                            inputClassName="mt-1 block w-full px-4 py-3 rounded-lg shadow-sm focus:outline-none transition duration-300 ease-in-out"
-                            required
-                        />
-                    </div>
-                    {error && (
-                        <p className="text-red-500 text-center text-sm">
-                            Failed to login. Please check your credentials.
-                        </p>
-                    )}
-                    <div className="flex items-center justify-center">
-                        <Button
-                            label={loading ? 'Loading...' : 'Submit'}
-                            disabled={loading}
-                            className="w-full py-3 bg-indigo-600 text-white rounded-lg shadow-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 transition duration-300 ease-in-out disabled:opacity-50"
-                            type="submit"
-                        />
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+   if (!token) {
+       return (
+           <div className="flex min-h-screen items-center justify-center bg-gradient-to-br px-4">
+               <div className="p-8 max-w-md w-full bg-white rounded-lg shadow-2xl">
+                   <h2 className="text-3xl font-bold  mb-8 text-center">
+                       Log In
+                   </h2>
+                   <form onSubmit={handleLogin} className="space-y-8">
+                       <div>
+                           <Input
+                               label="Username"
+                               type="text"
+                               value={username}
+                               onChange={(e) => setUsername(e.target.value)}
+                               placeholder="Enter your username"
+                               className="mt-4"
+                               labelClassName="text-black"
+                               inputClassName="mt-1 block w-full px-4 py-3 rounded-lg shadow-sm focus:outline-none transition duration-300 ease-in-out"
+                               required
+                           />
+                       </div>
+                       <div>
+                           <Input
+                               label="Password"
+                               type="password"
+                               value={password}
+                               onChange={(e) => setPassword(e.target.value)}
+                               placeholder="Enter your password"
+                               className="mt-4"
+                               labelClassName="text-black"
+                               inputClassName="mt-1 block w-full px-4 py-3 rounded-lg shadow-sm focus:outline-none transition duration-300 ease-in-out"
+                               required
+                           />
+                       </div>
+                       {error && (
+                           <p className="text-red-500 text-center text-sm">
+                               Failed to login. Please check your credentials.
+                           </p>
+                       )}
+                       <div className="flex items-center justify-center">
+                           <Button
+                               label={loading ? 'Loading...' : 'Submit'}
+                               disabled={loading}
+                               className="w-full py-3 bg-indigo-600 text-white rounded-lg shadow-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 transition duration-300 ease-in-out disabled:opacity-50"
+                               type="submit"
+                           />
+                       </div>
+                   </form>
+               </div>
+           </div>
+       )
+   }
 };
